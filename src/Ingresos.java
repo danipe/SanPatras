2bb2a829d356ef46dd0037a8cdd8e9589d82a139
sergio <sergiomorenoalbert@gmail.com>
2017-02-08 16:30:16 +0100

    Initial

--- src/Ingresos.java
+++ src/Ingresos.java
@@ -1,4 +1,68 @@
-
 public class Ingresos {
+	private int codIngreso;
+	private int codMedico;
+	private int codPaciente;
+	private String numHabitacion;
+	private int numCama;
+	private String fechaIngreso;
 
+	public Ingresos(){
+		this.codIngreso = 0;
+		this.codMedico = 0;
+		this.codPaciente = 0;
+		this.numHabitacion = "";
+		this.numCama = 0;
+		this.fechaIngreso = "";
+	}
+
+	/*
+	 * Getter & Setters
+	 */
+	public int getCodIngreso() {
+		return codIngreso;
+	}
+
+	public void setCodIngreso(int codIngreso) {
+		this.codIngreso = codIngreso;
+	}
+
+	public int getCodMedico() {
+		return codMedico;
+	}
+
+	public void setCodMedico(int codMedico) {
+		this.codMedico = codMedico;
+	}
+
+	public int getCodPaciente() {
+		return codPaciente;
+	}
+
+	public void setCodPaciente(int codPaciente) {
+		this.codPaciente = codPaciente;
+	}
+
+	public String getNumHabitacion() {
+		return numHabitacion;
+	}
+
+	public void setNumHabitacion(String numHabitacion) {
+		this.numHabitacion = numHabitacion;
+	}
+
+	public int getNumCama() {
+		return numCama;
+	}
+
+	public void setNumCama(int numCama) {
+		this.numCama = numCama;
+	}
+
+	public String getFechaIngreso() {
+		return fechaIngreso;
+	}
+	
+	public void setFechaIngreso(String fechaIngreso) {
+		this.fechaIngreso = fechaIngreso;
+	}	
 }
--- src/Principal.java
+++ src/Principal.java
@@ -1,5 +1,13 @@
-
+import java.util.ArrayList;
 public class Principal {
+	//Array lists de ingresos,medicos y pacientes
+	static ArrayList<String> ingresos = new ArrayList<String>();
+	static ArrayList<String> medicos = new ArrayList<String>();
+	static ArrayList<String> pacientes = new ArrayList<String>();
+	//Llamamos a las otras clases
+	Ingresos i = new Ingresos();
+	Medicos m = new Medicos();
+	Pacientes p = new Pacientes();
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
