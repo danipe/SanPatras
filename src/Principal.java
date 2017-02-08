5c6553337f40cc27add927fa184ccc98b0500e51
sergio <sergiomorenoalbert@gmail.com>
2017-02-08 16:54:36 +0100

    Añadido el menu

--- src/Principal.java
+++ src/Principal.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 public class Principal {
 	//Array lists de ingresos,medicos y pacientes
 	static ArrayList<String> ingresos = new ArrayList<String>();
@@ -6,12 +7,29 @@ public class Principal {
 	static ArrayList<String> pacientes = new ArrayList<String>();
 	//Llamamos a las otras clases
 	Ingresos i = new Ingresos();
-	
 	Pacientes p = new Pacientes();
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Medico m = new Medico(0);
 	}
+	
+	/**
+	 * Menu para elegir la opción al usuario 
+	 * @return la opcion del usuario
+	 */
+	public static int menu(){
+		Scanner menu = new Scanner(System.in);
+        System.out.println("Elige una de estas opciones: ");
+        System.out.println("-------------------------\n");
+        System.out.println("1 - Insertar paciente");
+        System.out.println("2 - Insertar medico");
+        System.out.println("3 - Nuevo ingreso");
+        System.out.println("4 - Salir");
+		System.out.println(">");
+		int opcion = menu.nextInt();
+		return opcion;
+	}
 
 }
+
